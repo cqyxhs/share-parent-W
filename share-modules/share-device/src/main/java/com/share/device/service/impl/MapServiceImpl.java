69ad7b051306a458823cbcd8add3189374af3604
cqyxhs <17268013654@163.com>
2026-05-20 16:31:10 +0800

    去除key

--- share-modules/share-device/src/main/java/com/share/device/service/impl/MapServiceImpl.java
+++ share-modules/share-device/src/main/java/com/share/device/service/impl/MapServiceImpl.java
@@ -22,7 +22,7 @@ public class MapServiceImpl implements IMapService {
     @Resource
     private RestTemplate restTemplate;
     @Value("${tencent.map.key}")
-    private String key = "H4LBZ-LQPLQ-SFU5O-4CPC5-OYDY6-7PBJ2";
+    private String key = "your-key";
 
     @Override
     public Double calculateDistance(String startLongitude,String startLatitude,String endLongitude,String endLatitude) {
